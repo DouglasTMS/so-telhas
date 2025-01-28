29f07d914623d5f4acef2b65351d9738c074c66b
Douglas Siebert <douglastms@outlook.com>
2025-01-28 00:47:28 -0400

    Instalação Google Adsense.

--- web/index.php
+++ web/index.php
@@ -74,6 +74,8 @@
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5182123534844534"
         crossorigin="anonymous"></script>
 
+
+
 </head>
 
 <body>
