bb5d3f813dd3d90db83c4411be920915e1ac81e5
Douglas Siebert <douglastms@outlook.com>
2025-02-10 00:37:05 -0400

    Correção de bug da modal de whatsapp-conversion.

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -205,7 +205,10 @@ $(function () {
                 }
 
                 if (e.success) {
-                    $(".whatsapp-conversion__lead-fields__main__message").fadeOut(100);
+                    $(".whatsapp-conversion__lead-fields__main__message").fadeOut(100, function (e) {
+                        $(".whatsapp-conversion__lead-fields__main__sellers-list").fadeIn(100).css("display", "flex");
+                        $(".whatsapp-conversion__lead-fields__header p").text("Selecione seu Atendente!");
+                    });
 
                     return;
                 }
