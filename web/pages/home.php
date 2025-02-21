77abf350d5bab99a09960fdb3266ce084b77dafa
Douglas Siebert <douglastms@outlook.com>
2025-02-21 00:57:17 -0400

    Alteração do slider.

--- web/pages/home.php
+++ web/pages/home.php
@@ -3,18 +3,10 @@
     <div class="slider__box style= width: 100%">
         <div class="slider__box__item style= width: 100%">
 
-            <!--
-            <div class="slider__box__item__box">
-                <h1>Só Telhas</h1>
-                <p>Cobrindo o Brasil com Aço</p>
-            </div>
-
--->
-
             <picture alt="">
-                <source srcset="<?= thumb()->make("web/assets/img/slider-01-desktop.png", 1920, 600) ?>" media="(min-width: 1024px)">
-                <source srcset="<?= thumb()->make("web/assets/img/slider-01-desktop.png", 1920, 600) ?>" media="(min-width: 500px)">
-                <img src="<?= thumb()->make("web/assets/img/slider-01-mobile.png", 1200, 1000) ?>" alt="Só Telhas - Cobrindo o Brasil com Aço">
+                <source srcset="<?= thumb()->make("web/assets/img/slider-01-desktop.jpg", 1920, 600) ?>" media="(min-width: 1024px)">
+                <source srcset="<?= thumb()->make("web/assets/img/slider-01-desktop.jpg", 1920, 600) ?>" media="(min-width: 500px)">
+                <img src="<?= thumb()->make("web/assets/img/slider-01-mobile.jpg", 1200, 1000) ?>" alt="Só Telhas - Cobrindo o Brasil com Aço">
             </picture>
 
         </div>
