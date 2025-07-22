26e54b478dca0bcdbd98461040dcaf89fc96557d
Douglas Siebert <douglastms@outlook.com>
2025-07-22 19:49:19 -0300

    Inclusão de Perguntas e Respostas.

--- web/inc/answers.php
+++ web/inc/answers.php
@@ -7,40 +7,39 @@
         </header>
 
         <article class="answers__box__item">
-            <p class="answers__box__item__question question">O que é Telha Isotérmica</p>
-            <p class="answers__box__item__response response">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia hic in tenetur quis libero fugiat voluptatum doloremque eos iste nihil alias facere ducimus quae dolorem, ad quos? Quidem, optio quo.</p>
+            <p class="answers__box__item__question question">O que é uma Telha Termoacústica?</p>
+            <p class="answers__box__item__response response">A Telha Termoacústica, também conhecida como Telha Isotérmica, é composta por duas chapas metálicas com um núcleo isolante (geralmente de EPS, PU ou PIR) entre elas. Esse núcleo proporciona isolamento térmico, ajudando a manter a temperatura interna do ambiente, e isolamento acústico, reduzindo ruídos externos, como o barulho da chuva. É uma opção ideal para quem busca conforto e economia de energia.</p>
         </article>
 
         <article class="answers__box__item">
-            <p class="answers__box__item__question question">O que é Telha Isotérmica</p>
-            <p class="answers__box__item__response response">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia hic in tenetur quis libero fugiat voluptatum doloremque eos iste nihil alias facere ducimus quae dolorem, ad quos? Quidem, optio quo.</p>
+            <p class="answers__box__item__question question">Quais são as vantagens das Telhas Isotérmicas?</p>
+            <p class="answers__box__item__response response">As telhas isotérmicas oferecem isolamento térmico e acústico, reduzindo a temperatura interna e o barulho de chuva. Elas também são mais resistentes e proporcionam maior durabilidade para o telhado.</p>
         </article>
 
         <article class="answers__box__item">
-            <p class="answers__box__item__question question">O que é Telha Isotérmica</p>
-            <p class="answers__box__item__response response">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia hic in tenetur quis libero fugiat voluptatum doloremque eos iste nihil alias facere ducimus quae dolorem, ad quos? Quidem, optio quo.</p>
+            <p class="answers__box__item__question question">A Só Telhas fabrica ou apenas revende os produtos?</p>
+            <p class="answers__box__item__response response">A Só Telhas é uma fábrica especializada na produção de telhas metálicas e isotérmicas, além de oferecer perfis, cumeeiras, fixadores e acabamentos de alta qualidade.</p>
         </article>
 
         <article class="answers__box__item">
-            <p class="answers__box__item__question question">O que é Telha Isotérmica</p>
-            <p class="answers__box__item__response response">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia hic in tenetur quis libero fugiat voluptatum doloremque eos iste nihil alias facere ducimus quae dolorem, ad quos? Quidem, optio quo.</p>
+            <p class="answers__box__item__question question">Vocês entregam para outras cidades e estados?</p>
+            <p class="answers__box__item__response response">Sim! Realizamos entregas para diversas regiões do Brasil, principalmente regiões próximas a Goiânia e Brasília garantindo que o cliente receba o produto com segurança e dentro do prazo combinado.</p>
         </article>
 
         <article class="answers__box__item">
-            <p class="answers__box__item__question question">O que é Telha Isotérmica</p>
-            <p class="answers__box__item__response response">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia hic in tenetur quis libero fugiat voluptatum doloremque eos iste nihil alias facere ducimus quae dolorem, ad quos? Quidem, optio quo.</p>
+            <p class="answers__box__item__question question">As Telhas Isotérmicas da Só Telhas podem ser usadas em qualquer tipo de construção?</p>
+            <p class="answers__box__item__response response">Sim! Elas são ideais para residências, comércios, indústrias, galpões e outros tipos de construções, proporcionando conforto e resistência em todos os casos.</p>
         </article>
 
         <article class="answers__box__item">
-            <p class="answers__box__item__question question">O que é Telha Isotérmica</p>
-            <p class="answers__box__item__response response">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia hic in tenetur quis libero fugiat voluptatum doloremque eos iste nihil alias facere ducimus quae dolorem, ad quos? Quidem, optio quo.</p>
+            <p class="answers__box__item__question question">Qual é a durabilidade das Telhas Metálicas e Isotérmicas?</p>
+            <p class="answers__box__item__response response">Com a manutenção correta, nossas telhas podem durar mais de 20 anos, já que são fabricadas com matérias-primas de alta qualidade e tecnologia avançada.</p>
         </article>
 
         <article class="answers__box__item">
-            <p class="answers__box__item__question question">O que é Telha Isotérmica</p>
-            <p class="answers__box__item__response response">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia hic in tenetur quis libero fugiat voluptatum doloremque eos iste nihil alias facere ducimus quae dolorem, ad quos? Quidem, optio quo.</p>
+            <p class="answers__box__item__question question">Como solicitar um orçamento na Só Telhas?</p>
+            <p class="answers__box__item__response response">Você pode solicitar um orçamento através do nosso WhatsApp, telefone ou visitando nossa loja física. Nossa equipe responderá rapidamente com uma proposta personalizada.</p>
         </article>
-
 
     </div>
 </section>
